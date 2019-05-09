63d5dc40ae6b48721f45bd40411c21913bbf4571
Joel <farrellsc@163.com>
2019-05-09 00:11:53 -0400

    Delete config.js

--- prototype/EventPages/js/config.js
+++
@@ -1,8 +0,0 @@
-AWS.config.update({
-	accessKeyId: "AKIATNRLE65RYB4BXKZV",
-	secretAccessKey: "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"
-});
-
-var s3 = new AWS.S3();
-
-var apigClient = apigClientFactory.newClient();
