3c30e714dd44c9af811a6a95cdb58831820ffd43
Joel <farrellsc@163.com>
2019-05-19 01:30:45 -0400

    Update config.js

--- prototype/EventPages/js/config.js
+++ prototype/EventPages/js/config.js
@@ -1,30 +1,3 @@
-config_list = {
-    tasktrigger: {
-        poolId: 'us-east-1_4pgPaolIC',
-        poolClientId: '2j7b8ui0m88c453ich2vd8hckc',
-        identityPoolId: 'us-east-1:094b02ca-c329-416a-9ddc-98ce893c9ad7',
-        region: 'us-east-1',
-    }
-};
-
-// Change Here!
-current_config = config_list['tasktrigger'];
-
-window.appconfig = {
-    cognito: {
-        // Cognito User Pool
-        userPoolId: current_config.poolId,
-        userPoolClientId: current_config.poolClientId,
-        identityPool: current_config.identityPoolId,
-        region: current_config.region
-    },
-    api: {
-        // url of API Gateway Deployment 
-        // LF0URL: current_config.LF0InvokeUrl,
-        // LF0Method1: current_config.LF0Method1
-    }
-};
-
 var PRIVATE_COLOR = "#247699";
 var FRIEND_COLOR = "#259652";
 var PUBLIC_COLOR = "#95a52c";
